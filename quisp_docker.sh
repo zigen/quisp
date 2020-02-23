e86c1411717f9e52f7a3791e0f47b7ade515a818
Chibikuri <ryosuke.satoh.wk@gmail.com>
2020-02-23 14:02:54 +0900

    easier shell

--- quisp_docker.sh
+++ quisp_docker.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-docker run --rm -it -v "$(pwd):/root/models" -u "$(id -u):$(id -g)" -u=0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$IP:0" omnetpp/omnetpp-gui:u18.04-5.6
+issocat=$(lsof -i:6000| grep socat| wc -l)
+pros=$(lsof -i:6000|wc -l)
+int1=1
+int2=2
+if [ -z "$DISPLAY" ]
+then
+    echo "Can't detect X11 desplay. If you can't detect where is your display, check ./doc/xhost_trouble_shooting.md"
+    exit
+else
+    # xterm
+    xterm -e "$(xhost $IP)"
+fi
+socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
+docker run --rm -it -v "$(pwd):/root/models" -u "$(id -u):$(id -g)" -u=0 -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="$IP:0" omnetpp/omnetpp-gui:u18.04-5.6.1
+trap "lsof -i:6000 -t|xargs kill" 0
