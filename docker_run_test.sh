98843a2229170c1a82636912478a850dae53d2eb
zigen <hrlclb@gmail.com>
2020-10-09 03:20:22 +0900

    fix

--- docker_run_test.sh
+++ docker_run_test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker run --rm -it --name quisp -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh -c "ls -la; pwd; cd /root; ls -la ; cd /root/quisp/test/; /bin/bash test.sh"
+docker run --rm -it --name quisp -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh <<-EOF
+    ls -la
+    pwd
+    cd /root
+    ls -la 
+    cd /root/quisp/test/
+    /bin/bash test.sh
+EOF
 # docker exec -it quisp /bin/bash
