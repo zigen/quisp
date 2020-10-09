ad92da54778a5ddf386fc4dd539b4355a4bec19a
zigen <hrlclb@gmail.com>
2020-10-09 12:37:13 +0900

    fix

--- docker_run_test.sh
+++ docker_run_test.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [ "$TRAVIS" = "true" ]; then
-    docker run --rm -it --name quisp -v "$TRAVIS_BUILD_DIR:/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh -c "cd /root/quisp/test; /bin/bash test.sh"
-fi
-
 docker run --rm -it --name quisp -v "$(pwd):/root/quisp" -u "$(id -u):$(id -g)" quisp /bin/sh -c "cd /root/quisp/test; /bin/bash test.sh"
 # docker exec -it quisp /bin/bash
